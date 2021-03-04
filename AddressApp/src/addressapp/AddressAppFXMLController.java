b61462138f5df760dd73864d43459654793bf97a
nervi0s <nervi0s@protonmail.com>
2021-03-04 19:23:16 +0100

    Ejercicio TableView

---
+++ AddressApp/src/addressapp/AddressAppFXMLController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addressapp;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * FXML Controller class
+ *
+ * @author CFGS
+ */
+public class AddressAppFXMLController implements Initializable {
+
+    @FXML
+    private Button buttonNew;
+    @FXML
+    private Button buttonEdit;
+    @FXML
+    private Button buttonDelete;
+    @FXML
+    private TableView<Persona> tableView;
+    @FXML
+    private TableColumn<Persona, String> tableColumnName;
+    @FXML
+    private TableColumn<?, ?> tableColumnLastName;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void crearPersona(ActionEvent event) {
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona("Pop", "Pop last", "calle 123", "Madrid", 2222, "19/19/19"));
+        ObservableList<Persona> lista = FXCollections.observableArrayList(personas);
+        tableView.setItems(lista);
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>(personas.get(0).getCity()));
+    }
+
+    @FXML
+    private void editarPersona(ActionEvent event) {
+    }
+
+    @FXML
+    private void EliminarPersona(ActionEvent event) {
+    }
+
+}
---
+++ AddressApp/src/addressapp/Persona.java
@@ -0,0 +1,45 @@
+package addressapp;
+
+public class Persona {
+
+    private final String name;
+    private final String lastName;
+    private String street;
+    private String city;
+    private int cp;
+    private final String birdthday;
+
+    public Persona(String name, String lastName, String street, String city, int cp, String birdthday) {
+        this.name = name;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.cp = cp;
+        this.birdthday = birdthday;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+}
