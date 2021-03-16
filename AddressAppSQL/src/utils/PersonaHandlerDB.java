3cdbfb56c07129c511ab8b98a13a351162a56a80
nervi0s <nervi0s@protonmail.com>
2021-03-16 23:24:45 +0100

    Modificado id incrementable, cambiada lógica de guardar personas, uso del nuevo constructor, método para actializar y comprobar modificaciones de personas

--- AddressAppSQL/src/utils/PersonaHandlerDB.java
+++ AddressAppSQL/src/utils/PersonaHandlerDB.java
@@ -22,6 +22,7 @@ public class PersonaHandlerDB {
                 Persona p = personaParser(res.getString("nombre"), res.getString("apellidos"), res.getString("calle"), res.getString("ciudad"), res.getInt("cp"), res.getString("fecha_nacimiento"));
                 //Seteamos a cada persona su correspondiente ID
                 p.setId(res.getInt("id_persona"));
+                Persona.idIncrement = res.getInt("id_persona");//En caso de que existan personas en la DB, asignamos a la variable estática el último id de la DB
                 //Añadimos la persona obtenido dela DB al ArrayList
                 personas.add(p);
             }
@@ -34,10 +35,15 @@ public class PersonaHandlerDB {
 
     public void savePersonasInDB(ArrayList<Persona> listaPersonas, Connection connection) {
         for (Persona persona : listaPersonas) {
-            if (!personaExistInDB(persona, connection)) {
+            if (!personaExistInDB(persona, connection)) {//Si la persona de la lista NO existe en la DB
                 insertPersona(persona, connection);
-            } else {
-                //No se guarda en la DB
+            } else {//Si la persona de la lista existe en la DB
+                if (personaWasModified(persona, connection)) {//Si la persona de la lista ha sido modificada
+                    System.out.println("persona modificada");
+                    updatePersona(persona, connection);
+                } else {//Si no hay cambios en la lista
+                    System.out.println("no hay cambios para guardar");
+                }
             }
         }
     }
@@ -47,18 +53,17 @@ public class PersonaHandlerDB {
         return parsedPerson;
     }
 
+    private Persona personaParser(int id, String name, String lastName, String street, String city, int cp, String birdthday) {
+        Persona parsedPerson = new Persona(id, name, lastName, street, city, cp, birdthday);
+        return parsedPerson;
+    }
+
     private boolean personaExistInDB(Persona persona, Connection connection) {
         boolean existe = false;
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM personas WHERE  id_persona = ? AND nombre = ? AND apellidos = ? AND calle = ? AND ciudad = ? AND cp = ? AND fecha_nacimiento = ?");
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM personas WHERE  id_persona = ?");
 
             ps.setInt(1, persona.getId());
-            ps.setString(2, persona.getName());
-            ps.setString(3, persona.getLastName());
-            ps.setString(4, persona.getStreet());
-            ps.setString(5, persona.getCity());
-            ps.setInt(6, persona.getCp());
-            ps.setString(7, persona.getBirdthday());
 
             ResultSet r = ps.executeQuery();
             existe = r.next();
@@ -66,6 +71,23 @@ public class PersonaHandlerDB {
             Logger.getLogger(PersonaHandlerDB.class.getName()).log(Level.SEVERE, null, ex);
         }
         return existe;
+    }
+
+    private boolean personaWasModified(Persona persona, Connection connection) {
+        boolean modified = false;
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM personas WHERE  id_persona = ?");
+
+            ps.setInt(1, persona.getId());
+
+            ResultSet r = ps.executeQuery();
+            r.next();
+            Persona parsedPerson = personaParser(r.getInt("id_persona"), r.getString("nombre"), r.getString("apellidos"), r.getString("calle"), r.getString("ciudad"), r.getInt("cp"), r.getString("fecha_nacimiento"));
+            modified = !(persona.esIgual(parsedPerson));
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonaHandlerDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return modified;
     }
 
     public void insertPersona(Persona persona, Connection c) {
@@ -79,6 +101,24 @@ public class PersonaHandlerDB {
             ps.setString(5, persona.getCity());
             ps.setInt(6, persona.getCp());
             ps.setString(7, persona.getBirdthday());
+
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonaHandlerDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updatePersona(Persona persona, Connection c) {
+        try {
+            PreparedStatement ps = c.prepareStatement("UPDATE personas SET nombre = ?, apellidos = ?, calle = ?, ciudad = ?, cp = ?, fecha_nacimiento = ? WHERE id_persona = ?");
+
+            ps.setString(1, persona.getName());
+            ps.setString(2, persona.getLastName());
+            ps.setString(3, persona.getStreet());
+            ps.setString(4, persona.getCity());
+            ps.setInt(5, persona.getCp());
+            ps.setString(6, persona.getBirdthday());
+            ps.setInt(7, persona.getId());
 
             ps.executeUpdate();
         } catch (SQLException ex) {
